d5969649810003f526ba7a67de4afa4a0ed9de46
Alexandre Soares <alexandre.asoares@gmail.com>
2015-03-01 11:57:37 -0300

    01.03.2015.5

--- Exemplos/Switch/Switch-03/app/build/generated/source/r/debug/com/example/switch_03/R.java
+++ Exemplos/Switch/Switch-03/app/build/generated/source/r/debug/com/example/switch_03/R.java
@@ -26,6 +26,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int txtFlash=0x7f050001;
+        public static final int txtVibrar=0x7f050002;
     }
     public static final class style {
         /** 
