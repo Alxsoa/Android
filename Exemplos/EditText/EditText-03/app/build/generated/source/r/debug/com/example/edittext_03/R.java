d2e933b121ff529d5ab5b2b515240c4b8ad96547
Alexandre Soares <alexandre.asoares@gmail.com>
2015-03-01 10:30:47 -0300

    01.03.2015.3

--- Exemplos/EditText/EditText-03/app/build/generated/source/r/debug/com/example/edittext_03/R.java
+++ Exemplos/EditText/EditText-03/app/build/generated/source/r/debug/com/example/edittext_03/R.java
@@ -11,30 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int btnOk=0x7f080004;
-        public static final int edtSenha=0x7f080001;
-        public static final int edtSenhaNumerica=0x7f080002;
-        public static final int txtView1=0x7f080000;
-        public static final int txtView2=0x7f080003;
+        public static final int btnOk=0x7f070004;
+        public static final int edtSenha=0x7f070001;
+        public static final int edtSenhaNumerica=0x7f070002;
+        public static final int txtView1=0x7f070000;
+        public static final int txtView2=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
-    public static final class menu {
-        public static final int main=0x7f070000;
-    }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f040000;
+        public static final int app_name=0x7f040001;
+        public static final int txtBotao=0x7f040002;
+        public static final int txtSenha=0x7f040003;
+        public static final int txtSenhaNumerica=0x7f040004;
     }
     public static final class style {
         /** 
@@ -45,9 +42,9 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050001;
     }
 }
